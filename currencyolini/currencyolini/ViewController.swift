213bd498817987d6c8b5f54d9380c9ea378ef3ac
Knut Nygaard <knutnyg@gmail.com>
2015-02-21 14:34:02 +0100

    use real currencies

--- currencyolini/currencyolini/ViewController.swift
+++ currencyolini/currencyolini/ViewController.swift
@@ -25,12 +25,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func convertButtonClicked(sender: UIButton) {
-        label.text = "Hello World";
         // Get user preference
         var test = NSUserDefaults.standardUserDefaults();
-        var conversionRate = test.integerForKey("slider_preference");
-        if let number = self.fromAmount.text?.toInt() {
-            self.toAmount.text = "\(number * conversionRate)"
+//        var conversionRate = test.integerForKey("slider_preference");
+        let fromCurrency = test.stringForKey("from_country")!
+        let toCurrency = test.stringForKey("to_country")!
+        println("http://rate-exchange.appspot.com/currency?from=\(fromCurrency)&to=\(toCurrency)")
+        let url = NSURL(string: "http://rate-exchange.appspot.com/currency?from=\(fromCurrency)&to=\(toCurrency)" )
+        let request = NSURLRequest(URL: url!)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
+            var error: NSError?
+            var boardsDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as NSDictionary
+            var conversionRate : Double = boardsDictionary.objectForKey("rate") as Double;
+            println(conversionRate)
+                    if let number = self.fromAmount.text?.toInt() {
+                      self.toAmount.text = "\(Double(number) * conversionRate)"
+                }
         }
     }
 
