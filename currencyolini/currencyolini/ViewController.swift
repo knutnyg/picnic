3a8b46c1951994e63143a502fc30646561cb67d4
Knut Nygaard <knut@Knuts-MacBook-Pro.local>
2015-02-21 11:34:33 +0100

    intial project files

---
+++ currencyolini/currencyolini/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  currencyolini
+//
+//  Created by Knut Nygaard on 21/02/15.
+//  Copyright (c) 2015 Knut Nygaard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
