6d9d1530b78a39cfe141f98066330ff3d4284fd3
Knut Nygaard <knutnyg@gmail.com>
2015-03-03 00:14:51 +0100

    tatt ut podfilene av git. De ble vist med i en commit

---
+++ picnic/ConversionManager.swift
@@ -0,0 +1,9 @@
+//
+//  ConversionManager.swift
+//  picnic
+//
+//  Created by Knut Nygaard on 3/3/15.
+//  Copyright (c) 2015 Knut Nygaard. All rights reserved.
+//
+
+import Foundation
---
+++ picnicTests/LoctionManagerTests.swift
@@ -0,0 +1,9 @@
+//
+//  LoctionManagerTests.swift
+//  picnic
+//
+//  Created by Knut Nygaard on 3/2/15.
+//  Copyright (c) 2015 Knut Nygaard. All rights reserved.
+//
+
+import Foundation
