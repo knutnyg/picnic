1ac45385393ca65eb2a5c12accfb2f744022b427
Knut Nygaard <knut@Knuts-MacBook-Pro.local>
2015-03-18 17:12:40 +0100

    fixed no internet crash

--- picnic/ConversionManager.swift
+++ picnic/ConversionManager.swift
@@ -18,24 +18,28 @@ class ConvertsionRateManager {
                 let request = NSURLRequest(URL: NSURL(string: urlString)!)
                 
                 NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
-                    var castedResponse = response as NSHTTPURLResponse
+
                     if error != nil {
                         promise.failure(NSError(domain: "ConvertionRateManagerAPIError", code: 503, userInfo: nil))
-                    } else {
+                    }
+                    
+                    if response != nil {
+                        var castedResponse = response as NSHTTPURLResponse
+                    
                         if(castedResponse.statusCode == 200){
                             var boardsDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
                             
                             var conversionRate : Double = boardsDictionary.objectForKey("rate") as Double;
                             promise.success(conversionRate)
+                            
                         } else {
                             promise.failure(NSError(domain: "CurrencyApiError", code: 503, userInfo: nil))
                         }
                     }
                 }
-                
             }
+
         }
-        
         return promise.future
     }
 }
