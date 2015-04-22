aafcd4d555caabf417ea8c893e73cb74d0deb52e
Knut Nygaard <knutnyg@gmail.com>
2015-04-22 20:05:10 +0200

    minor tweeks

---
+++ commit_release.sh
@@ -0,0 +1,5 @@
+cd ..
+echo Commiting version
+git add picnic/Info.plist
+git commit -m "Bumping version number for release!"
+git push
--- increment_build_number.sh
+++ increment_build_number.sh
@@ -1,10 +1,7 @@
+cd ..
 git pull
-
 buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "picnic/Info.plist")
 buildNumber=$(($buildNumber + 1))
 echo Incrementing buildnumber from $((buildNumber-1)) to $buildNumber
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "picnic/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" picnic/Info.plist
 
-git add picnic/Info.plist
-git commit -m "Bumping version number for release!"
-#git push
