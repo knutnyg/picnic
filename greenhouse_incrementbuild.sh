1f39f70aba857f018e99ec826779fce0956e8c34
Knut Nygaard <knut@Knuts-iMac.local>
2016-10-26 19:26:04 +0200

    Fix

--- greenhouse_incrementbuild.sh
+++ greenhouse_incrementbuild.sh
@@ -4,4 +4,4 @@ buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "Pic
 
 echo Adding CI postfix to buildnumber to buildnumber from buildNumber to $buildNumber.$GREENHOUSE_BUILD_NUMBER
 
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber.$GREENHOUSE_BUILD_NUMBER" Portfolio/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber.$GREENHOUSE_BUILD_NUMBER" Picnic/Info.plist
